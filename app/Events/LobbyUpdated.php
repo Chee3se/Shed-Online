bbe52b2c33a126bc7e0a2cec88522f24f51ba421
eaterz <muraskoruslans@gmail.com>
2024-12-17 10:05:25 +0200

    lobby

---
+++ app/Events/LobbyUpdated.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Lobby;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class LobbyUpdated implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $lobby;
+
+    public function __construct(Lobby $lobby)
+    {
+        $this->lobby = $lobby;
+    }
+
+    public function broadcastOn()
+    {
+        return new Channel('lobbies');
+    }
+
+    public function broadcastWith()
+    {
+        return [
+            'lobby_id' => $this->lobby->id,
+            'current_players' => $this->lobby->current_players,
+            'players' => $this->lobby->players->map(function ($player) {
+                return [
+                    'id' => $player->id,
+                    'name' => $player->name
+                ];
+            })
+        ];
+    }
+}
