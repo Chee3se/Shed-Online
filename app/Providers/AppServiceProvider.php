34dacbef3957c307a6d6e17dd34be1969ad664b3
Chee3se <emilspete999@gmail.com>
2025-01-07 10:27:56 +0200

    fix?

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,6 +25,7 @@ class AppServiceProvider extends ServiceProvider
         if (config('app.env') === 'production') {
             URL::forceScheme('https');
             if (!extension_loaded('pcntl.so')) {
+                ini_set('enable_dl', 'On');
                 dl('pcntl.so');
             }
         }
