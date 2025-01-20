513848d9b399708c7c6c5c81b1d5a7b79b06a6dc
Chee3se <emilspete999@gmail.com>
2025-01-20 20:34:40 +0200

    names

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -11,9 +11,14 @@ class GameController
 {
 
     public function index(Request $request, $code){
-
         return Inertia::render('Multiplayer', [
             'code' => $code
         ]);
+    }
+    public function play(Request $request, $code){
+    }
+    public function draw(Request $request, $code){
+    }
+    public function take(Request $request, $code){
     }
 }
