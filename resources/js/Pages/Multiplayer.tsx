3d15c3b3d066549842b0d1bb06981035f853d27c
Chee3se <emilspete999@gmail.com>
2025-01-30 10:24:38 +0200

    fix?

--- resources/js/Pages/Multiplayer.tsx
+++ resources/js/Pages/Multiplayer.tsx
@@ -19,15 +19,6 @@ export default function Multiplayer({ auth, code, lobby }: { auth: any; code: st
     const [gameStarted, setGameStarted] = useState<boolean>(false);
 
     useEffect(() => {
-        const savedDeckId = localStorage.getItem('deckId');
-        const savedGameCode = localStorage.getItem('gameCode');
-
-        if (savedDeckId && savedGameCode === code) {
-            deckId.current = savedDeckId;
-            setGameStarted(true);
-        } else {
-            generateNewDeck();
-        }
 
         window.axios.defaults.headers.common['X-Socket-ID'] = window.Echo.socketId();
 
